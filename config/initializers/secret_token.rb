2696176de7ebee01086f977105ee96cad6ac0999
Cristian Veller <cristianveller@xyze.it>
2013-07-23 10:46:22 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helloworld::Application.config.secret_key_base = '6ca5b7ceec3ae766680594627bc5ce445057053e0e92d7971bd9cf957196ab1e15dd241fbe1a858e87b9859101f8dc1f9fb03123b08754b83161418c842f8f01'
